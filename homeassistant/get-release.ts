4db4eb1e05aa8a6c690e8fa61667803307c5bf37
Breno Salles <git@brenosalles.com>
2025-09-27 09:11:04 +0100

    feat: Offer homeassistant+hacs in a docker image

---
+++ homeassistant/get-release.ts
@@ -0,0 +1,13 @@
+import { Octokit } from "npm:octokit";
+import { createActionAuth } from "npm:@octokit/auth-action";
+
+const octokit = new Octokit({
+  authStrategy: createActionAuth,
+});
+
+const { data: release } = await octokit.rest.repos.getLatestRelease({
+  owner: "home-assistant",
+  repo: "core",
+});
+
+console.log(release.tag_name)
